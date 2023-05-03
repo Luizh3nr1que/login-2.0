7631566dbf401d79d28ea236157a8ebc127d0d5e
luizh3nrique <luizh3nrique00@gmail.com>
2023-05-03 11:53:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('.form');
+const erro = document.querySelector('.erro');
+const button = document.querySelector('.btnEntrar');
+const input = document.querySelectorAll('input');
+
+
+button.addEventListener('click', () => {
+    let camposVazio = false;
+    input.forEach(input => {
+        if (input.value === '') {
+            camposVazio = true;
+        }
+    });
+
+    if (camposVazio) {
+        erro.innerHTML = 'Preencha todos os campos'
+    }
+})
